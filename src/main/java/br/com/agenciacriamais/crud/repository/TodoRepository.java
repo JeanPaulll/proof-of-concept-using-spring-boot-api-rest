24e2ddc392220239ba6f18625f3cd03c5decb9ee
Jean Paul <jeanpaulwebb@gmail.com>
2022-04-05 02:36:26 -0300

    fix: Configuring in-memory database

--- src/main/java/br/com/agenciacriamais/crud/repository/TodoRepository.java
+++ src/main/java/br/com/agenciacriamais/crud/repository/TodoRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.agenciacriamais.crud.model.Todo;
 
+/**
+ * @author Jean Paul - <jeanpaulwebb@gmail.com>
+ * @class TodoRepository
+ * @description Class responsible for the 'TODO'
+ * @date 05/04/2022
+ */
 public interface TodoRepository extends JpaRepository<Todo, Long>{
 
 }

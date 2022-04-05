fa2c4df4dd942a3747455e9ab115ed4015bd50f5
Jean Paul <jeanpaulwebb@gmail.com>
2022-04-05 02:07:09 -0300

    fix: CommandLineRunner run specific code when application starts.

--- src/main/java/br/com/agenciacriamais/crud/AgenciaCriaMaisApplication.java
+++ src/main/java/br/com/agenciacriamais/crud/AgenciaCriaMaisApplication.java
@@ -1,10 +1,38 @@
 package br.com.agenciacriamais.crud;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.agenciacriamais.crud.model.Todo;
+import br.com.agenciacriamais.crud.repository.TodoRepository;
 
 @SpringBootApplication
 public class AgenciaCriaMaisApplication {
+	
+	
+	@Autowired
+	private TodoRepository todoRepository; 
+	
+	@Bean
+	public CommandLineRunner init() {
+		
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				Todo todo  = new Todo();
+				todo.setDescription("Criando uma task");
+				todo.setCreatedDate(LocalDateTime.now());
+				todoRepository.save(todo);
+			}
+		};
+		
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AgenciaCriaMaisApplication.class, args);
--- src/main/java/br/com/agenciacriamais/crud/repository/TodoRepository.java
+++ src/main/java/br/com/agenciacriamais/crud/repository/TodoRepository.java
@@ -1,5 +1,9 @@
 package br.com.agenciacriamais.crud.repository;
 
-public interface TodoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.agenciacriamais.crud.model.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long>{
 
 }

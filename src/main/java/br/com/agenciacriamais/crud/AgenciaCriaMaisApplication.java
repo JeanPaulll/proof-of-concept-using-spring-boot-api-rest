af73469e202b04ef00e2c15a1b0eec25a29e19ee
Jean Paul <jeanpaulwebb@gmail.com>
2022-04-05 01:11:16 -0300

    feat: Initial commit to install the framework

---
+++ src/main/java/br/com/agenciacriamais/crud/AgenciaCriaMaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.agenciacriamais.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaCriaMaisApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaCriaMaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agenciacriamais/crud/AgenciaCriaMaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agenciacriamais.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaCriaMaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

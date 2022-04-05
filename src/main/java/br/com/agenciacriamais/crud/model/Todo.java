8e18f2311b858ae20dd4d020a3803ca357d6da70
Jean Paul <jeanpaulwebb@gmail.com>
2022-04-05 01:31:57 -0300

    fix: Class responsible for the 'TODO'

--- src/main/java/br/com/agenciacriamais/crud/model/Todo.java
+++ src/main/java/br/com/agenciacriamais/crud/model/Todo.java
@@ -1,5 +1,36 @@
 package br.com.agenciacriamais.crud.model;
 
-public class Todo {
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Jean Paul - <jeanpaulwebb@gmail.com>
+ * @class Todo
+ * @description Class responsible for the 'TODO'
+ * @date 05/04/2022
+ */
+@Entity
+public class Todo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String description;
+	
+	@Column
+	private Boolean done;
+	
+	@Column 
+	private LocalDateTime createdDate;
+	
+	@Column 
+	private LocalDateTime doneDate;
+	
 }
